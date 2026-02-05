f0a0c56cc587c9aa80f2d6f91f9fc8d6cf50b855
Irakli Skhirtladze <irskhirtladze@tbcbank.com.ge>
2026-02-05 10:28:36 +0400

    changes

--- ml_training/src/ml_training/main.py
+++ ml_training/src/ml_training/main.py
@@ -9,7 +9,7 @@ from transformers import (TrOCRProcessor, VisionEncoderDecoderModel, Seq2SeqTrai
 from ml_training.dataset import GeorgianTokenizer, GeorgianOCRDataset
 from ml_training.setup import setup_environment, check_env
 from ml_training.training.training_loop import train_model, save_final_model
-from ml_training.training.validation import compute_metrics_seq2seq
+# from ml_training.training.validation import compute_metrics_seq2seq
 
 
 def main() -> None:
@@ -55,63 +55,63 @@ def main() -> None:
     # set up device and run training loop
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model.to(device)
-    # train_model(
-    #     paths,
-    #     model,
-    #     train_loader,
-    #     test_loader,
-    #     loader_generator,
-    #     device,
-    #     tokenizer,
-    #     epochs=3,
-    #     save_every=1000,
-    #     max_grad_norm=1.0,
-    #     learning_rate=1e-6,
-    #     resume_latest=True
-    # )
-    # save_final_model(paths, model, processor, tokenizer)
-
-    # ============= seq2seqtrainer version =============
-    seq2seq_training_args = Seq2SeqTrainingArguments(
-        predict_with_generate=True,
-
-        eval_strategy='steps',
-        eval_steps=1000,
-        logging_strategy="steps",
-        logging_steps=1000,
-
-        save_strategy='steps',
-        save_steps=1000,
-        save_total_limit=2,
-
-        load_best_model_at_end=True,
-        metric_for_best_model='cer',
-        greater_is_better=False,
-
+    train_model(
+        paths,
+        model,
+        train_loader,
+        test_loader,
+        loader_generator,
+        device,
+        tokenizer,
+        epochs=3,
+        save_every=1000,
+        max_grad_norm=1.0,
         learning_rate=1e-6,
-        weight_decay=0.01,
-        per_device_train_batch_size=16,
-        per_device_eval_batch_size=16,
-        fp16=True,
-        output_dir=str(paths.output_dir),
-        num_train_epochs=3,
-        report_to='tensorboard',
+        resume_latest=True
     )
+    save_final_model(paths, model, processor, tokenizer)
 
-    metrics_with_tokenizer = partial(compute_metrics_seq2seq, tokenizer=tokenizer)
-    seq2sec_trainer = Seq2SeqTrainer(
-        model=model,
-        processing_class=tokenizer,
-        args=seq2seq_training_args,
-        compute_metrics=metrics_with_tokenizer,
-        train_dataset=train_dataset,
-        eval_dataset=test_dataset,
-        data_collator=default_data_collator
-    )
-
-    print("Starting Seq2Seq Training...")
-    result = seq2sec_trainer.train()
-    seq2sec_trainer.save_model(str(paths.output_dir / "best_model_final"))
+    # # ============= seq2seqtrainer version =============
+    # seq2seq_training_args = Seq2SeqTrainingArguments(
+    #     predict_with_generate=True,
+    #
+    #     eval_strategy='steps',
+    #     eval_steps=1000,
+    #     logging_strategy="steps",
+    #     logging_steps=1000,
+    #
+    #     save_strategy='steps',
+    #     save_steps=1000,
+    #     save_total_limit=2,
+    #
+    #     load_best_model_at_end=True,
+    #     metric_for_best_model='cer',
+    #     greater_is_better=False,
+    #
+    #     learning_rate=1e-6,
+    #     weight_decay=0.01,
+    #     per_device_train_batch_size=16,
+    #     per_device_eval_batch_size=16,
+    #     fp16=True,
+    #     output_dir=str(paths.output_dir),
+    #     num_train_epochs=3,
+    #     report_to='tensorboard',
+    # )
+    #
+    # metrics_with_tokenizer = partial(compute_metrics_seq2seq, tokenizer=tokenizer)
+    # seq2sec_trainer = Seq2SeqTrainer(
+    #     model=model,
+    #     processing_class=tokenizer,
+    #     args=seq2seq_training_args,
+    #     compute_metrics=metrics_with_tokenizer,
+    #     train_dataset=train_dataset,
+    #     eval_dataset=test_dataset,
+    #     data_collator=default_data_collator
+    # )
+    #
+    # print("Starting Seq2Seq Training...")
+    # result = seq2sec_trainer.train()
+    # seq2sec_trainer.save_model(str(paths.output_dir / "best_model_final"))
 
 
 if __name__ == "__main__":
