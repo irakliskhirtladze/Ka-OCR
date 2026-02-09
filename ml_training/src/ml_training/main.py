66f1188dd25afa2658cae3e8ac991f5456c8bf40
Irakliskhirtladze <irskhirtladze@gmail.com>
2026-02-09 13:20:21 +0400

    changes

--- ml_training/src/ml_training/main.py
+++ ml_training/src/ml_training/main.py
@@ -1,15 +1,13 @@
-from functools import partial
 import pandas as pd
 import torch
 from sklearn.model_selection import train_test_split
 from torch.utils.data import DataLoader
-from transformers import (TrOCRProcessor, VisionEncoderDecoderModel, Seq2SeqTrainingArguments, Seq2SeqTrainer,
-    default_data_collator)
+from transformers import (TrOCRProcessor, VisionEncoderDecoderModel)
 
 from ml_training.dataset import GeorgianTokenizer, GeorgianOCRDataset
 from ml_training.setup import setup_environment, check_env
+from ml_training.training.seq2seq import init_seq2seq_trainer
 from ml_training.training.training_loop import train_model, save_final_model
-# from ml_training.training.validation import compute_metrics_seq2seq
 
 
 def main() -> None:
@@ -49,73 +47,37 @@ def main() -> None:
                               num_workers=2 if check_env() == "colab" else 6)
     test_loader = DataLoader(test_dataset, batch_size=16, shuffle=False)
 
-    # Quick check of online augmented samples
-    from ml_training.utils import save_debug_samples
-    if check_env() == "colab":
-        save_debug_samples(train_loader, tokenizer, str(paths.drive_output_dir / "debug_augs"))
-    elif check_env() == "local":
-        save_debug_samples(train_loader, tokenizer, str(paths.output_dir / "debug_augs"))
+    # # Quick check of online augmented samples
+    # from ml_training.utils import save_debug_samples
+    # if check_env() == "colab":
+    #     save_debug_samples(train_loader, tokenizer, str(paths.drive_output_dir / "debug_augs"))
+    # elif check_env() == "local":
+    #     save_debug_samples(train_loader, tokenizer, str(paths.output_dir / "debug_augs"))
 
-    # # set up device and run training loop
-    # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    # model.to(device)
-    # train_model(
-    #     paths,
-    #     model,
-    #     train_loader,
-    #     test_loader,
-    #     loader_generator,
-    #     device,
-    #     tokenizer,
-    #     epochs=20,
-    #     save_every=1000,
-    #     max_grad_norm=1.0,
-    #     learning_rate=1e-5,
-    #     resume_latest=True
-    # )
-    # save_final_model(paths, model, processor, tokenizer)
+    # set up device and run training loop
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model.to(device)
+    train_model(
+        paths,
+        model,
+        train_loader,
+        test_loader,
+        loader_generator,
+        device,
+        tokenizer,
+        epochs=20,
+        save_every=1000,
+        max_grad_norm=1.0,
+        learning_rate=1e-5,
+        resume_latest=True
+    )
+    save_final_model(paths, model, processor, tokenizer)
 
 
     # # ============= seq2seqtrainer version =============
-    # seq2seq_training_args = Seq2SeqTrainingArguments(
-    #     predict_with_generate=True,
-    #
-    #     eval_strategy='steps',
-    #     eval_steps=1000,
-    #     logging_strategy="steps",
-    #     logging_steps=1000,
-    #
-    #     save_strategy='steps',
-    #     save_steps=1000,
-    #     save_total_limit=2,
-    #
-    #     load_best_model_at_end=True,
-    #     metric_for_best_model='cer',
-    #     greater_is_better=False,
-    #
-    #     learning_rate=1e-6,
-    #     weight_decay=0.01,
-    #     per_device_train_batch_size=16,
-    #     per_device_eval_batch_size=16,
-    #     fp16=True,
-    #     output_dir=str(paths.output_dir),
-    #     num_train_epochs=3,
-    #     report_to='tensorboard',
-    # )
-    #
-    # metrics_with_tokenizer = partial(compute_metrics_seq2seq, tokenizer=tokenizer)
-    # seq2sec_trainer = Seq2SeqTrainer(
-    #     model=model,
-    #     processing_class=tokenizer,
-    #     args=seq2seq_training_args,
-    #     compute_metrics=metrics_with_tokenizer,
-    #     train_dataset=train_dataset,
-    #     eval_dataset=test_dataset,
-    #     data_collator=default_data_collator
-    # )
-    #
     # print("Starting Seq2Seq Training...")
-    # result = seq2sec_trainer.train()
+    # seq2sec_trainer = init_seq2seq_trainer()
+    # seq2sec_trainer.train()
     # seq2sec_trainer.save_model(str(paths.output_dir / "best_model_final"))
 
 
--- ml_training/src/ml_training/training/seq2seq.py
+++ ml_training/src/ml_training/training/seq2seq.py
@@ -1,1 +1,69 @@
+import torch.nn
+from transformers import (TrOCRProcessor, VisionEncoderDecoderModel, Seq2SeqTrainingArguments, Seq2SeqTrainer,
+                          default_data_collator, EvalPrediction)
+from functools import partial
+import evaluate
+
+from ml_training.dataset import GeorgianTokenizer, GeorgianOCRDataset
+from ml_training.setup import Paths
+
+
+cer_metric = evaluate.load("cer")
+
+
+def init_args(paths: Paths) -> Seq2SeqTrainingArguments:
+    return Seq2SeqTrainingArguments(
+        predict_with_generate=True,
+
+        eval_strategy='steps',
+        eval_steps=1000,
+        logging_strategy="steps",
+        logging_steps=1000,
+
+        save_strategy='steps',
+        save_steps=1000,
+        save_total_limit=2,
+
+        load_best_model_at_end=True,
+        metric_for_best_model='cer',
+        greater_is_better=False,
+
+        learning_rate=1e-6,
+        weight_decay=0.01,
+        per_device_train_batch_size=16,
+        per_device_eval_batch_size=16,
+        fp16=True,
+        output_dir=str(paths.output_dir),
+        num_train_epochs=3,
+        report_to='tensorboard',
+    )
+
+
+def compute_metrics_seq2seq(pred: EvalPrediction, tokenizer: GeorgianTokenizer) -> dict[str, float]:
+    labels_ids = pred.label_ids
+    pred_ids = pred.predictions
+
+    # Decode predictions and labels
+    # -100 is the ignore index we set in our Dataset
+    labels_ids[labels_ids == -100] = tokenizer.pad_token_id
+    pred_str = tokenizer.batch_decode(pred_ids)
+    label_str = tokenizer.batch_decode(labels_ids)
+
+    cer = cer_metric.compute(predictions=pred_str, references=label_str)
+
+    return {"cer": cer}
+
+
+def init_seq2seq_trainer(tokenizer: GeorgianTokenizer, model: torch.nn.Module, seq2seq_args: Seq2SeqTrainingArguments,
+                        train_dataset: GeorgianOCRDataset, test_dataset: GeorgianOCRDataset) -> Seq2SeqTrainer:
+    metrics_with_tokenizer = partial(compute_metrics_seq2seq, tokenizer=tokenizer)
+    return Seq2SeqTrainer(
+        model=model,
+        processing_class=tokenizer,
+        args=seq2seq_args,
+        compute_metrics=metrics_with_tokenizer,
+        train_dataset=train_dataset,
+        eval_dataset=test_dataset,
+        data_collator=default_data_collator
+    )
 
