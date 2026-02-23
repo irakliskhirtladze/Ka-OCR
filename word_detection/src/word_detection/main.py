d00ee7dc091500a5b8a0b7d6326b8b6dc5ef5aff
Irakliskhirtladze <irskhirtladze@gmail.com>
2026-02-23 16:57:10 +0400

    changes

--- word_detection/src/word_detection/main.py
+++ word_detection/src/word_detection/main.py
@@ -6,11 +6,12 @@ from random import randint, choice
 
 from word_detection.data_factory.augmentation import augment_doc, test_online_aug
 from word_detection.data_factory.doc_gen import generate_docs, zip_dataset, dataset_to_hf
-from word_detection.utils import PATHS
+from word_detection.utils import PATHS, setup_environment
 from word_detection.yolo_train.training_loop import train
 
 
 def main() -> None:
+    setup_environment()
     train()
 
 
