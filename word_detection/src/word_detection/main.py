1a1e5cc83e21ca2c21331a972e31b6b54cd11172
Irakliskhirtladze <irskhirtladze@gmail.com>
2026-02-23 16:54:41 +0400

    changes

--- word_detection/src/word_detection/main.py
+++ word_detection/src/word_detection/main.py
@@ -7,10 +7,11 @@ from random import randint, choice
 from word_detection.data_factory.augmentation import augment_doc, test_online_aug
 from word_detection.data_factory.doc_gen import generate_docs, zip_dataset, dataset_to_hf
 from word_detection.utils import PATHS
+from word_detection.yolo_train.training_loop import train
 
 
 def main() -> None:
-    pass
+    train()
 
 
 if __name__ == '__main__':
--- word_detection/src/word_detection/utils.py
+++ word_detection/src/word_detection/utils.py
@@ -1,3 +1,5 @@
+import os
+import subprocess
 from dataclasses import dataclass
 from pathlib import Path
 
@@ -7,6 +9,12 @@ class Paths:
     base_dir: Path = Path(__file__).resolve().parent.parent.parent
     dataset_dir: Path = base_dir / "dataset"
     zip_path: Path = dataset_dir / "YOLO_ka_words.zip"
+    output_dir: Path = base_dir / "output"
+    checkpoints_dir: Path = base_dir / "checkpoints"
+
+    drive_dataset_zip_path: Path = base_dir / "drive/MyDrive/Colab Notebooks/word_detection/data/YOLO_ka_words.zip"
+    drive_output_dir: Path = base_dir / "drive/MyDrive/Colab Notebooks/word_detection/output",
+    drive_checkpoints_dir: Path = base_dir / "drive/MyDrive/Colab Notebooks/word_detection/checkpoints",
 
     env_path: Path = base_dir / ".env"
 
@@ -15,6 +23,45 @@ class Paths:
 
     def __post_init__(self):
         self.dataset_dir.mkdir(parents=True, exist_ok=True)
+        self.output_dir.mkdir(parents=True, exist_ok=True)
+        self.checkpoints_dir.mkdir(parents=True, exist_ok=True)
 
 
 PATHS = Paths()
+
+
+def check_env() -> str:
+    """Determines if running in Colab, or Locally"""
+    # Colab-specific check
+    # Check for 'google.colab' module or unique Colab env var
+    if 'COLAB_RELEASE_TAG' in os.environ or 'COLAB_BACKEND_VERSION' in os.environ:
+        return "colab"
+
+    return "local"
+
+
+def setup_environment() -> None:
+    """
+    Setup environment for model training depending on where the session is running.
+    Returns dataclass instance with paths to dataset, checkpoints, outputs, etc.
+    """
+    env = check_env()
+    print(f"running on {env}")
+
+    if check_env() == "colab":  # set up colab session with google drive for permanent storage
+        print(f"base dir: {PATHS.base_dir}")
+        if not PATHS.dataset_dir.exists():
+            print("\nExtracting dataset zip from Drive to session storage...")
+            try:
+                # -j: junk paths (do not create internal folders);-q "quiet", don't print filenames;
+                # -o overwrite existing files; -d specifies the destination dir
+                cmd = ["unzip", "-j", "-o", "-q", str(PATHS.drive_dataset_zip_path), "-d", str(PATHS.dataset_dir)]
+                subprocess.run(cmd, capture_output=True, text=True, check=True)
+                print("Extraction successful!")
+            except subprocess.CalledProcessError as e:
+                print(f"Extraction failed!\nError: {e.stderr}")
+        else:
+            print("Dataset already copied to colab session.")
+
+    else:  # Setup for local session
+        pass
